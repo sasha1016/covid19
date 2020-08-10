67fa52c257c5406137e10347f0571902dbead974
shashank pradeep <shashankpradeep5@gmail.com>
2020-08-11 01:57:55 +0530

    Created new join function and updated row_mostly_null

--- Classes/Row.py
+++ Classes/Row.py
@@ -1,13 +1,10 @@
-import os 
-import camelot
-import PyPDF2 as pypdf
-import re
 import pandas as pd
 import numpy as np
 
 def row_mostly_null(row):
-    most_frequent_value = row.value_counts().index[0]
-    if most_frequent_value == 'nan' or most_frequent_value == np.nan:
+    counts = row.value_counts(dropna=False)
+    most_frequent_value = counts.index[0]
+    if (most_frequent_value == 'nan' or pd.isna(most_frequent_value)) and counts.to_list()[0] != 1 :
         return True
     else:
         return False
@@ -32,4 +29,19 @@ class Rows():
 
     def __init__(self):
         print('asd')
+
+
+def join(df,delimiters=None):
+    name = 'Row Join'
+    disjoint_rows = [index for index,row in df.iterrows() if row_mostly_null(row)]
+    for row in disjoint_rows:
+        df.loc[row] = df.loc[row].replace({np.nan:'','nan':''})
+        df.loc[row-1] = df.loc[row-1].apply(str) + ' ' + df.loc[row].apply(str)
+        if delimiters is not None:
+            for column in delimiters:
+                if df.at[row,column] != '':
+                    df.at[row-1,column] = (df.at[row-1,column] + delimiters[column] + df.at[row,column])
+        df.loc[row] = np.nan
+    df.dropna(how='all',axis=0,inplace=True)
+    return (name,'joined all disjoint rows',disjoint_rows)
         
